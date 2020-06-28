a911a15a2831f1dcc0a752ac49f18cc1a05753fc
locth <locth@uit.edu.vn>
2020-06-28 20:04:54 +0700

    Query only set of emp_no

--- redis_query.py
+++ redis_query.py
@@ -7,10 +7,14 @@ r = redis.Redis(
     password=''
 )
 
-pipe = r.pipeline()
-
+print("Start querying...")
 start_time = time.time()
-for emp in r.smembers('employees'):
-    pipe.hgetall(emp)
-pipe.execute()
+r.smembers('employees')
 print("[REDIS QUERY] Execution time: %s" % (time.time() - start_time))
+
+# To query detail of each record, run the code below:
+# pipe = r.pipeline()
+
+# for emp in r.smembers('employees'):
+#     pipe.hgetall(emp)
+# pipe.execute()
