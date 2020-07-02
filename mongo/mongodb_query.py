0ca4ffcd869cb0bcae7ca7f69b31664f758300f2
locth <locth@uit.edu.vn>
2020-07-02 23:27:20 +0700

    Change number of query in mongo

--- mongo/mongodb_query.py
+++ mongo/mongodb_query.py
@@ -9,16 +9,23 @@ db = client['employees']
 print("--- WORKING ON 300.024 RECORDS ---")
 emp_collection = db['employees']
 
+emp_list = emp_collection.find({}, {"_id":1})
+
 print("Querying 300.024 records...")
 start_time = time.time()
-emp_collection.find()
+for doc in emp_list:
+    emp_collection.find({"_id":doc["_id"]})
 print("[MONGO QUERY] Execution time: %s" %(time.time()-start_time))
+
+print([doc["emp_no"] for doc in emp_list])
 
 print("--- WORKING ON 2.844.047 RECORDS ---")
 sala_collection = db['salaries']
 
+sala_list = sala_collection.find({}, {"_id":1})
+
 print("Querying 2.844.047 records...")
 start_time = time.time()
-sala_collection.find()
+for doc in sala_list:
+    sala_collection.find({"_id":doc["_id"]})
 print("[MONGO QUERY] Execution time: %s" %(time.time()-start_time))
-
